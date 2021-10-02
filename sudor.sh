307275d0c27f9b04abee89f4e47869f632440cd2
ikoral <ilhankoral@outlook.com>
2021-10-02 23:24:56 +0200

    sudor.xml file updated

---
+++ sudor.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+pwd
+rm sudor.xml
+curl -o sudor.xml https://b2b.sudor.com.tr/xml/integration/E9874E13AEC/NGA6P4SFBL66AJPRMWZUCPK4G
+git add .
+git commit -m 'sudor.xml file updated'
+git push origin main
