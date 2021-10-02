074df3f2e6e2d3afaa883f24082a06c595f41f6e
ikoral <ilhankoral@outlook.com>
2021-10-03 00:08:36 +0200

    sudor.xml file updated

--- sudor.sh
+++ sudor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm sudor.xml
+rm /home/caca/temp/sudor_xml/sudor.xml
 pwd
-curl -o sudor.xml https://b2b.sudor.com.tr/xml/integration/E9874E13AEC/NGA6P4SFBL66AJPRMWZUCPK4G
-git add .
-git commit -m 'sudor.xml file updated'
-git push origin main
+curl -o /home/caca/temp/sudor_xml/sudor.xml https://b2b.sudor.com.tr/xml/integration/E9874E13AEC/NGA6P4SFBL66AJPRMWZUCPK4G
+cd /home/caca/temp/sudor_xml && git add .
+cd /home/caca/temp/sudor_xml && git commit -m 'sudor.xml file updated'
+cd /home/caca/temp/sudor_xml && git push origin main
