4fd8b8438284e847956ad61019e36870b9ed828b
ikoral <caca@_HOSTNAME_>
2022-03-01 00:34:49 +0100

    sudor.xml file updated

--- sudor.sh
+++ sudor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -f /home/caca/temp/sudor_xml/sudor.xml
 pwd
-curl -o /home/caca/temp/sudor_xml/sudor.xml https://b2b.sudor.com.tr/xml/integration/E9874E13AEC/NGA6P4SFBL66AJPRMWZUCPK4G
+curl -o /home/caca/temp/sudor_xml/sudor.xml https://b2b.sudor.com.tr/xml/integration/E9874E13AEC/YDW6OFPN0AAVBZS5R696N7EKZ
 cd /home/caca/temp/sudor_xml && git add .
 cd /home/caca/temp/sudor_xml && git commit -m 'sudor.xml file updated'
 cd /home/caca/temp/sudor_xml && git push origin main
